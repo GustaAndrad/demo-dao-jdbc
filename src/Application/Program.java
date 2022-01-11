e3b4266069b0c482a88ff5734751d171b254e61f
Gustavo Andrade <gusta.andradev@gmail.com>
2022-01-11 13:20:22 -0300

    findAll implementation

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -26,5 +26,11 @@ public class Program {
         for (Seller obj : list){
             System.out.println(obj);
         }
+        
+        System.out.println("=== Test 3 :  seller findAll ==="); 
+        list = sellerDao.findAll();
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }
