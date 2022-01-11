723648c03de718fab7ac25e325b5d6828ce6defd
Gustavo Andrade <gusta.andradev@gmail.com>
2022-01-11 09:56:15 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package model.dao;
+
+import java.util.List;
+import model.entities.Department;
+
+/**
+ *
+ * @author gusta
+ */
+public interface DepartmentDao {
+    
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+    
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package model.dao;
+
+import java.util.List;
+import model.entities.Seller;
+
+/**
+ *
+ * @author gusta
+ */
+public interface SellerDao {
+    
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+    
+}
